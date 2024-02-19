9481822a5888561ceb990fb2decb3d739f35cecf
Johann Steffanno Jerônimo da Costa Caniato Amorim <johannsamorim@gmail.com>
2024-02-19 20:06:51 -0300

    Using a Datframe to obtain the percentage of memory consumption

--- mem.py
+++ mem.py
@@ -8,22 +8,22 @@ df = df.map(lambda x: str(x).lower())  ##df in lower casa
 
 MCU = sys.argv[1] # ex. atmega328p, attiny
 
-# # Nome do arquivo de saída (.elf)
+# Output filename (.elf)
 output_elf = "build/main.elf"
 
-# # Comando avr-size
+# avr-size command
 avr_size_cmd = ["avr-size", output_elf]
 
-# # Executa o comando avr-size
+# execute avr-size command
 avr_size_output = subprocess.check_output(avr_size_cmd, universal_newlines=True)
 
-# # Divide as linhas de saída em uma lista
+# Spite the output lines
 output_lines = avr_size_output.splitlines()
 
-# # Extrai a linha relevante contendo os tamanhos
+# Extract the relevant line that contains the values
 info_line = output_lines[1]
 
-# # Extrai os tamanhos das colunas
+# Extract the column sizes
 text_size = int(info_line.split()[0])
 data_size = int(info_line.split()[1])
 bss_size = int(info_line.split()[2])
@@ -41,7 +41,7 @@ Total_RAM_memory = int(float(Total_RAM_memory[0])*1024)
 Flash_percentage = round((float(Flash_memory/Total_Flash_memory)*100), 2)
 Data_Memory_percentage = round((float(data_memory)/Total_RAM_memory)*100, 2)
 
-# Exibe os tamanhos das seções
+# Show the infos
 print(f"Flash: {Flash_memory}/{Total_Flash_memory} bytes => {Flash_percentage}%/100%")
 print(f"RAM: {data_memory}/{Total_RAM_memory} bytes => {Data_Memory_percentage}%/100%")
 
